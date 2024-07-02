e1f0c18a409853d1b43ff8186ec5a6ecec37a897
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2024-07-02 19:49:55 +0000

    fix(migrate): migrate

--- src/infra/functions/commands/migrate.ts
+++
@@ -1,6 +0,0 @@
-import { execSync } from 'child_process';
-import { Handler } from 'aws-lambda';
-
-export const handler: Handler = async () => {
-  await execSync('npm run migrate:prod', { stdio: 'inherit' });
-};
